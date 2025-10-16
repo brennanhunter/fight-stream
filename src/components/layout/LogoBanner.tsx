8e3c988e52a8199087c4643f82f0633ab8ad2b54
Hunter Coleman <40704537+brennanhunter@users.noreply.github.com>
2025-10-16 09:42:01 -0400

    Removed logo border.

--- src/components/layout/LogoBanner.tsx
+++ src/components/layout/LogoBanner.tsx
@@ -6,7 +6,7 @@ export default function LogoBanner() {
           <img 
             src="/logos/logo-white-text.png" 
             alt="BoxStreamTV Logo" 
-            className="h-[70px] md:h-[88px] w-auto border-2 border-primary rounded-lg p-2 pl-[50px] shadow-[0_0_15px_rgba(220,38,38,0.6)] hover:shadow-[0_0_25px_rgba(220,38,38,0.9)] hover:scale-105 transition-all duration-300 cursor-pointer"
+            className="h-[70px] md:h-[88px] w-auto p-2 pl-[50px]"
           />
         </div>
       </div>
