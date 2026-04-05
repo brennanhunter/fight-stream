f7e71f49aca2cc36ecaa9f42005a22ce5538b783
Hunter Coleman <huntercoleman@users.noreply.github.com>
2026-04-05 13:12:14 -0400

    Still on inngest.

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -2,9 +2,9 @@ import { serve } from 'inngest/next';
 import { inngest } from '@/lib/inngest';
 import { eventReminderFunction, eventStartingFunction } from '@/inngest/functions';
 
+export const maxDuration = 300;
+
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [eventReminderFunction, eventStartingFunction],
-  serveOrigin: 'https://boxstreamtv.com',
-  servePath: '/api/inngest',
 });
